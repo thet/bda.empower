79262076f5b5c052693e8587559f73778dba22d1
Jens W. Klein <jk@kleinundpartner.at>
2018-11-08 18:56:18 +0100

    basic types

---
+++ src/bda/empower/browser/workspace.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+
+
+class WorkspaceView(BrowserView):
+
+    def workspace_title(self):
+        return ''.join(self.request['TraversalRequestNameStack'])
--- src/bda/empower/content/contribution.py
+++ src/bda/empower/content/contribution.py
@@ -1,60 +1,31 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
-from plone.dexterity.content import Container
-
-# from plone.namedfile import field as namedfile
+from bda.empower import _
+from plone.app.textfield import RichText
 from plone.supermodel import model
+from zope import schema
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
 
-
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
-
-
-# from bda.empower import _
+workspace_vocabulary = SimpleVocabulary(
+    [
+        SimpleTerm("case", title=u"Case"),
+        SimpleTerm("inherited", title=u"Inherited"),
+        SimpleTerm("analyse", title=u"Analyse"),
+        SimpleTerm("strategie", title=u"Strategie"),
+        SimpleTerm("action", title=u"Action"),
+    ]
+)
 
 
 class IContribution(model.Schema):
     """ Marker interface and Dexterity Python Schema for Contribution
     """
 
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    text = RichText(title=_(u"Text"), required=False)
 
-    # model.load('contribution.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    workspace = schema.Choice(
+        title=u"Workspace",
+        required=True,
+        vocabulary=workspace_vocabulary,
+        default="unbekannt",
+    )
