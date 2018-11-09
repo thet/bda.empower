5ed8da196bf783d801da2f9128e2aa0dc810e5dc
Jens W. Klein <jk@kleinundpartner.at>
2018-11-09 09:58:34 +0100

    basic first sketch

--- src/bda/empower/behaviors/contribution.py
+++ src/bda/empower/behaviors/contribution.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from bda.empower import _
 from plone.app.textfield import RichText
+from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
 from zope import schema
+from zope.interface import provider
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
@@ -17,8 +19,9 @@ workspace_vocabulary = SimpleVocabulary(
 )
 
 
-class IContribution(model.Schema):
-    """ Marker interface and Dexterity Python Schema for Contribution
+@provider(IFormFieldProvider)
+class IContributionBehavior(model.Schema):
+    """ Schema Only Behavior Contribution
     """
 
     text = RichText(title=_(u"Text"), required=False)
@@ -27,5 +30,5 @@ class IContribution(model.Schema):
         title=u"Workspace",
         required=True,
         vocabulary=workspace_vocabulary,
-        default="unbekannt",
+        default="inherited",
     )
