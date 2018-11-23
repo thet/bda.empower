2e3ab918b7096aba1ed671c30fd841e226872b85
Jens W. Klein <jk@kleinundpartner.at>
2018-11-23 11:55:56 +0100

    no at check

--- src/bda/empower/__init__.py
+++ src/bda/empower/__init__.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+
+# NO ARCHETYPES CHECKER
+try:
+    import Products.Archetypes
+
+    Products.Archetypes
+except ImportError:
+    pass
+else:
+    raise RuntimeError("Archetypes detected")
