11699819c2360f2a0eff0e14751ab6f3e97a925c
Jens W. Klein <jk@kleinundpartner.at>
2018-11-26 16:04:26 +0100

    fiux rolesetter

--- src/bda/empower/rolesetter.py
+++ src/bda/empower/rolesetter.py
@@ -7,26 +7,38 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-ROLENAME = "Expert"
 
-
-def update_expert_assigned_local_roles(obj, event):
-    """ subcriber on ICOntribution
-
-    to be configured to be called on ObjectCreated and ObjectModified
-    """
-    logger.info("run subscriber update_expert_assigned_local_roles")
-    # remove all users with local role "AKI EC Assigned" on parent (TN)
-    for username in obj.users_with_local_role(ROLENAME):
-        api.user.revoke_roles(username=username, obj=obj, roles=[ROLENAME])
+def _revoke_roles(obj, rolename):
+    """Remove all users with given local role"""
+    for username in obj.users_with_local_role(rolename):
+        api.user.revoke_roles(username=username, obj=obj, roles=[rolename])
         fingerpointing_log(
             "revoke_role_expert_assigned", username=username, id=obj.UID()
         )
-    users = obj.assigned_experts or ""
-    # set for einzel_coach'es from Verlauf local role "AKI EC Assigned"
-    for username in users.split(";"):
-        api.user.grant_roles(username=username, obj=obj, roles=[ROLENAME])
+
+
+def _set_role_for_users(obj, users, rolename):
+    """Set for role for given users on obj"""
+    for username in users:
+        api.user.grant_roles(username=username, obj=obj, roles=[rolename])
         fingerpointing_log(
             "grant_role_experts_assigned", username=username, id=obj.UID()
         )
+
+
+def _update_role_on_obj_using_users_from_field(obj, fieldname, rolename):
+    logger.info("run subscriber update_expert_assigned_local_roles")
+    _revoke_roles(obj, rolename)
+    users = getattr(obj, fieldname, "") or ""
+    users = users.split(";")
+    _set_role_for_users(obj, users, rolename)
     obj.reindexObjectSecurity()
+
+
+def update_expert_assigned_local_roles(obj, event):
+    """ subcriber
+    - to be configured to be called on ObjectCreated and ObjectModified
+    """
+    _update_role_on_obj_using_users_from_field(
+        obj, "experts_assigned", "Expert"
+    )
