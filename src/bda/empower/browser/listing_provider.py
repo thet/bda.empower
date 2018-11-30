32bd08fa054af3124c85da595f591a8817f27957
Jens W. Klein <jk@kleinundpartner.at>
2018-11-30 14:48:19 +0100

    base listing

--- src/bda/empower/browser/listing_provider.py
+++ src/bda/empower/browser/listing_provider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_parent
 from bda.empower import discourse
+from plone import api
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.contentprovider.provider import ContentProviderBase
 
@@ -15,7 +16,7 @@ class ListingProvider(ContentProviderBase):
 
     def update(self):
 
-        self.workspace_roots = {}
+        self.workspace_roots = []
         for brain in discourse.get_all_workspace_roots(
             self.context, self.current_def[0]
         ):
@@ -25,12 +26,17 @@ class ListingProvider(ContentProviderBase):
             record["previous"] = aq_parent(record["current"])
             record["following"] = []
             for brain in discourse.get_next_workspaces(record["current"]):
-                record["next"].append(brain.getObject())
+                record["following"].append(brain.getObject())
 
     def render(self):
         return self.template()
 
     def workspace_url(self, node):
-        return "{0}/ws/{1}#{2}".format(
-            self.context.absolute_url(), node.workspace, node.getId()
+        url = "{0}/ws/{1}/{2}".format(
+            self.context.absolute_url(),
+            node.workspace,
+            api.content.get_uuid(discourse.get_root_of_workspace(node)),
         )
+        if not discourse.is_workspace_root(node):
+            url = "{0}#{1}".format(url, node.getId())
+        return url
