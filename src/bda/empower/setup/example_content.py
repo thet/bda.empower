ba654a0c9215f58c22478917ef72a808ba0fcc2a
Johannes Raggam <thetetet@gmail.com>
2019-01-04 16:05:43 +0100

    Refactor example content creation: - add missing ws definition on case and contribution. - add client, coordinator and expert_pool on contribution. - add expert assignment on some nodes. - better - broader and deeper - threading.

--- src/bda/empower/setup/example_content.py
+++ src/bda/empower/setup/example_content.py
@@ -1,82 +1,136 @@
 # -*- coding: utf-8 -*-
+from .setupdevusers import DEV_USERS
+from bda.empower import discourse
 from collective.contentcreator import create_item_runner
 from collective.contentcreator import load_json
-from random import choice
-from random import randint
+from Products.CMFPlone.utils import safe_unicode
 from zope.component.hooks import getSite
 
 import loremipsum
+import plone.api
+import random
 
 
-max_items = 0
-cnt_items = 0
-max_depth = 0
+def create_text():
+    txt = []
+    cnt_bold = random.randint(1, 4)
+    cnt_italic = random.randint(1, 4)
+    for cnt in range(random.randint(1, 4)):
+        txt_ = loremipsum.get_paragraph()
+        if cnt == cnt_italic:
+            txt_ = u"<em>{0}</em>".format(txt_)
+        if cnt == cnt_bold:
+            txt_ = u"<strong>{0}</strong>".format(txt_)
+        txt_ = u"<p>{0}</p>".format(txt_)
+        txt.append(txt_)
+    return "\n".join(txt)
 
 
-def create_thread():
-    global max_items
-    global cnt_items
+def create_content():
+    cases = []
+    for it in range(4):
+        cases.append(create_case())
+    __import__('pdb').set_trace()
+    return cases
 
-    max_items = randint(1, 40)
-    cnt_items = 0
 
-    def _create(subtree, type_, depth=0):
-        global cnt_items
-        global max_depth
+def create_case():
+    wsdefs = discourse.get_workspace_definitions()
+    workspace = wsdefs.keys()[0]
 
-        choices = [1, 1, 2, 2, 3, 4]
-        if cnt_items:
-            choices += [0, 0, 0]
-        for cnt in range(choice(choices)):
+    users = plone.api.user.get_users()
+    users = map(lambda it: it.id, users)
+    random.shuffle(users)
+    client = users.pop()
+    coordinator = users.pop()
 
-            if type_ == "Case":
-                max_depth = randint(1, 6)
+    item = {
+        "@type": "Case",
+        "title": loremipsum.get_sentence(),
+        "text": create_text(),
+        "items": create_thread(workspace, users),
+        "workspace": workspace,
+        "client": safe_unicode(client),
+        "coordinators": safe_unicode(coordinator),
+        "expert_pool": u';'.join(users)
+    }
 
-            if depth > max_depth:
-                break
+    return item
 
-            depth += 1
 
-            if cnt_items >= max_items:
-                break
+def create_thread(current_workspace, expert_pool):
 
-            cnt_items += 1
+    workspace_definitions = discourse.get_workspace_definitions()
 
-            txt = []
-            for cnt in range(randint(1, 4)):
-                txt_ = loremipsum.get_paragraph()
-                if cnt == 0:
-                    txt_ = u"<p><strong>{0}</strong></p>".format(txt_)
-                elif cnt == 1:
-                    txt_ = "<p><em>{0}</em></p>".format(txt_)
-                else:
-                    txt_ = "<p>{0}</p>".format(txt_)
-                txt.append(txt_)
+    def _create(parent_workspace, expert_pool, cnt, max_cnt, depth, max_depth):
+        depth += 1
+        if depth > max_depth:
+            return []
+
+        thread = []
+
+        for _cnt in range(random.randint(2, 6)):  # min 2 answers per thread
+
+            cnt += 1
+            if cnt > max_cnt:
+                return thread
+
+            current_workspace = parent_workspace
+            experts_assigned = None
+            if random.choice([1, 2, 3]) == 1:
+                current_workspace = random.choice(
+                    workspace_definitions[parent_workspace]['next']
+                )
+                experts_assigned = random.sample(
+                    expert_pool,
+                    random.randint(1, 3)
+                )
 
             item = {
-                "@type": type_,
+                "@type": "Contribution",
                 "title": loremipsum.get_sentence(),
-                "text": "\n".join(txt),
-                "items": [],
+                "text": create_text(),
+                "items": _create(
+                    current_workspace,
+                    expert_pool,
+                    cnt,
+                    max_cnt,
+                    depth,
+                    max_depth
+                ),
+                "workspace": current_workspace,
             }
+            if experts_assigned:
+                item['experts_assigned'] = u';'.join(experts_assigned)
 
-            if cnt_items < max_items:
-                _create(item["items"], "Contribution")
+            thread.append(item)
 
-            subtree.append(item)
+        return thread
 
-    tree = []
-    _create(tree, "Case")
-
-    return tree
+    thread = _create(
+        current_workspace,
+        expert_pool,
+        0,
+        10,
+        0,
+        random.randint(4, 6)
+    )
+    return thread
 
 
 def example_content(context):
-    # content_structure = load_json('example_content.json', __file__)
 
+    for record in DEV_USERS:
+        try:
+            plone.api.user.create(**record)
+        except ValueError:
+            # user already exists.
+            pass
+
+    # content_structure = load_json('example_content.json', __file__)
     create_item_runner(
         getSite().cases,  # "Cases" is aleady created.
-        create_thread(),
+        create_content(),
         default_lang="de",
         default_wf_action="publish",
     )
