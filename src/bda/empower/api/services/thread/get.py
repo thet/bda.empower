cb526597812485921807cd948ef0a4831b30c43e
Johannes Raggam <thetetet@gmail.com>
2019-01-22 17:23:15 +0100

    add info for next/previous workspaces

--- src/bda/empower/api/services/thread/get.py
+++ src/bda/empower/api/services/thread/get.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_base
+from Acquisition import aq_parent
 from bda.empower import discourse
 from plone import api
 from plone.restapi.interfaces import IExpandableElement
@@ -18,7 +19,21 @@ class Thread(object):
         self.request = request
 
     def _make_item(self, item):
-        ob = aq_base(item.getObject())
+        ob = item.getObject()
+        ob_base = aq_base(ob)
+
+        previous = None
+        parent = aq_parent(ob)
+        if parent.portal_type in discourse.NODE_TYPES and\
+            aq_base(parent).workspace != ob_base.workspace:
+
+            previous = '/'.join(parent.getPhysicalPath())
+
+        next = []
+        for child in ob.contentValues():
+            if aq_base(child).workspace != ob_base.workspace:
+                next.append('/'.join(child.getPhysicalPath()))
+
         ret = {
             "@id": item.getURL(),
             "@type": item.PortalType(),
@@ -28,16 +43,16 @@ class Thread(object):
             "creator": item.Creator(),
             "created": item.CreationDate(),
             "modified": item.ModificationDate(),
-            "workspace": getattr(ob, 'workspace', None),
-            "client": getattr(ob, 'client', None),
-            "coordinators": getattr(ob, 'coordinators', None),
-            "expert_pool": getattr(ob, 'expert_pool', None),
-            "experts_assigned": getattr(ob, 'experts_assigned', None),
-            "previous_workspace": None,
-            "next_workspace": None,
+            "workspace": getattr(ob_base, 'workspace', None),
+            "client": getattr(ob_base, 'client', None),
+            "coordinators": getattr(ob_base, 'coordinators', None),
+            "expert_pool": getattr(ob_base, 'expert_pool', None),
+            "experts_assigned": getattr(ob_base, 'experts_assigned', None),
+            "previous_workspace_item": previous,
+            "next_workspace_items": next,
         }
 
-        text = getattr(ob, 'text', None)
+        text = getattr(ob_base, 'text', None)
         # TODO: check if output_relative_to(workspace) is better..?
         ret["text"] = text.output_relative_to(ob) if text else None
 
@@ -47,22 +62,6 @@ class Thread(object):
     def itemtree(self):
         items = discourse.get_workspace_tree(self.context)
         tree = discourse.build_tree(items)
-
-        roots = discourse.get_all_workspace_roots(self.context, getattr(self.context, 'workspace', None)  # TODO: fix. needs other way to get current workspace name - context might be case.  # noqa
-        nexts = []
-        for root in roots:
-            nexts.append(list(
-                discourse.get_next_workspaces(root.getObject())
-            ))
-
-        def _make_roots(item):
-
-            return {
-                'url': None,
-                'title': None
-            }
-
-        roots = map(_make_roots, roots)
 
         for key, items in tree.items():
             tree[key] = map(self._make_item, items)
