1b7c27e5baefeee305a2e107c92cdf48b64e4451
Johannes Raggam <thetetet@gmail.com>
2019-02-06 19:35:00 +0100

    add docker setup, remove dev-thet in favor of dev.cfg

---
+++ docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+COMMANDS="debug help logtail show stop adduser fg kill quit run wait console foreground logreopen reload shell status"
+START="start restart"
+CMD="/plone/buildout/bin/instance"
+
+echo "Plone Control Script"
+
+if [[ $START == *"$1"* ]]; then
+  _stop() {
+    $CMD stop
+    kill -TERM $child 2>/dev/null
+  }
+
+  trap _stop SIGTERM SIGINT
+  $CMD start
+  $CMD logtail &
+
+  child=$!
+  wait "$child"
+else
+  if [[ $COMMANDS == *"$1"* ]]; then
+    exec bin/instance "$@"
+  fi
+  exec "$@"
+fi
