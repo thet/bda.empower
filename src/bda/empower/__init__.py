a03b15eebb92c0c4aca0d7b27301e38a9340e063
Johannes Raggam <thetetet@gmail.com>
2019-09-29 02:35:45 +0200

    increase batch size as I dont want to implement a threading-supporting batching navigation/algorithm

--- src/bda/empower/__init__.py
+++ src/bda/empower/__init__.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 
+# Raise batch size to almost unlimited.
+# We do not want to deal with context.items batching YET.
+import plone.restapi.batching
+plone.restapi.batching.DEFAULT_BATCH_SIZE = 10000
+
 # NO ARCHETYPES CHECKER
 try:
     import Products.Archetypes
