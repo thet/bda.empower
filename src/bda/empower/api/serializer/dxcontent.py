38d37261b6e248d80d84b600866be6a41ce35e84
Johannes Raggam <thetetet@gmail.com>
2019-09-29 02:36:27 +0200

    add object permissions to API content

--- src/bda/empower/api/serializer/dxcontent.py
+++ src/bda/empower/api/serializer/dxcontent.py
@@ -8,6 +8,44 @@ from plone.restapi.serializer import dxcontent
 from zope.component import adapter
 from zope.interface import implementer
 
+import plone.api
+
+
+def filter_permissions(perms):
+    """Filter for bda.empower permissions and other important permissions.
+    """
+
+    #   id="bda.empower.AddContribution"
+    #   title="bda.empower: Add Contribution"
+    #   id="bda.empower.AddCase"
+    #   title="bda.empower: Add Case"
+    #   id="bda.empower.ModifyClient"
+    #   title="bda.empower: Modify Client"
+    #   id="bda.empower.ModifyExpertPool"
+    #   title="bda.empower: Modify Expert Pool"
+    #   id="bda.empower.ModifyCoordinators"
+    #   title="bda.empower: Modify Coordinators"
+    #   id="bda.empower.ModifyExpertsAssigned"
+    #   title="bda.empower: Modify Experts Assigned"
+    #   id="bda.empower.ModifyWorkspaceType"
+    #   title="bda.empower: Modify Workspace Type"
+
+    default_perms = [
+        "View",
+        "Add portal content",
+        "Modify portal content",
+        "Delete objects",
+        "bda.empower: Add Contribution",
+        "bda.empower: Add Case",
+        "bda.empower: Modify Client",
+        "bda.empower: Modify Expert Pool",
+        "bda.empower: Modify Coordinators",
+        "bda.empower: Modify Experts Assigned",
+        "bda.empower: Modify Workspace Type",
+    ]
+    perms = {p: v for p, v in perms.items() if p in default_perms}
+    return perms
+
 
 @implementer(ISerializeToJson)
 @adapter(IDexterityContent, IBdaEmpowerLayer)
@@ -28,8 +66,10 @@ class SerializeToJson(dxcontent.SerializeToJson):
 
         current_case = discourse.get_initial_root(self.context)
         result['current_case'] = current_case.absolute_url() if current_case else None
-
         result['workspace_root'] = discourse.is_workspace_root(self.context)
+        result['permissions'] = filter_permissions(
+            plone.api.user.get_permissions(obj=self.context)
+        )
 
         return result
 
@@ -53,7 +93,9 @@ class SerializeFolderToJson(dxcontent.SerializeFolderToJson):
 
         current_case = discourse.get_initial_root(self.context)
         result['current_case'] = current_case.absolute_url() if current_case else None
-
         result['workspace_root'] = discourse.is_workspace_root(self.context)
+        result['permissions'] = filter_permissions(
+            plone.api.user.get_permissions(obj=self.context)
+        )
 
         return result
