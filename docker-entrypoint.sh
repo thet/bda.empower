7f02585c94af6a6c23d282b9a67a72b3549755f1
Johannes Raggam <thetetet@gmail.com>
2019-10-01 01:09:57 +0200

    logail make these errors: "cant do nonzero end-relative seeks"

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 COMMANDS="debug help logtail show stop adduser fg kill quit run wait console foreground logreopen reload shell status"
 START="start restart"
-CMD="/plone/bin/instance"
+CMD="su-exec plone /plone/bin/instance"
 
 echo "Plone Control Script"
 
@@ -14,8 +14,7 @@ if [[ $START == *"$1"* ]]; then
   }
 
   trap _stop SIGTERM SIGINT
-  $CMD start
-  $CMD logtail &
+  $CMD console &
 
   child=$!
   wait "$child"
